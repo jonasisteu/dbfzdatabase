7ab24295d0fca40a63dcb2e51e13725fd80220ae
Jonasi STEU <jonasisteu@gmail.com>
2025-04-14 10:47:41 +0200

    [FIX] -> Front-end folder

---
+++ front-end/src/app/interfaces/character.ts
@@ -0,0 +1,12 @@
+export interface Character {
+
+    id: number;
+    reference: string;
+    name: string;
+    season: string;
+    originSeries: string;
+    archetype: string;
+    splashArtUrl: string;
+    thumbnail: string;
+
+}
