be1d1c8119485c7a460b78ffcc46db00b776bfa7
Jonasi STEU <jonasisteu@gmail.com>
2025-04-17 00:19:07 +0200

    [ADD] -> Responsive & background image, V1 should be done, now

--- front-end/src/app/components/navbar/navbar.component.ts
+++ front-end/src/app/components/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatIconModule, RouterLink, RouterOutlet],
+  imports: [MatToolbarModule, MatIconModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
